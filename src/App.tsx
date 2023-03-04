80964602d9c04f0d5f6b531dbc71263abd408ec9
Alexsus <alexsuslin@inbox.ru>
2023-03-04 13:53:02 +0300

    Add Files APP and Index

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import Site from "./Site/Site";
+
+function App() {
+  return (
+    <div className="App">
+    <Site/>
+
+    </div>
+  );
+}
+
+export default App;
